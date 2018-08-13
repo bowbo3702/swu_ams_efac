03a9eb362e31f4a27d82a563cb7ffc9f43b48fbf
bowbo3702 <bowbo3702@hotmail.com>
2018-08-13 22:09:16 +0700

    20180813-0002:finger

--- src/pages/setfinger/setfinger.ts
+++ src/pages/setfinger/setfinger.ts
@@ -143,7 +143,31 @@ export class SetfingerPage {
    return result;
   }
   CallFingerPrintAndroid(){
-
+    this.androidFingerprintAuth.isAvailable()
+    .then((result)=> {
+      if(result.isAvailable){
+        // it is available
+  
+        this.androidFingerprintAuth.encrypt({ clientId: 'myAppName', username: 'myUsername', password: 'myPassword' })
+          .then(result => {
+             if (result.withFingerprint) {
+                 console.log('Successfully encrypted credentials.');
+                 console.log('Encrypted credentials: ' + result.token);
+             } else if (result.withBackup) {
+               console.log('Successfully authenticated with backup password!');
+             } else console.log('Didn\'t authenticate!');
+          })
+          .catch(error => {
+             if (error === this.androidFingerprintAuth.ERRORS.FINGERPRINT_CANCELLED) {
+               console.log('Fingerprint authentication cancelled');
+             } else console.error(error)
+          });
+  
+      } else {
+        // fingerprint auth isn't available
+      }
+    })
+    .catch(error => console.error(error));
   }
   presentToast(sMsg) {
     let toast = this.toast.create({
